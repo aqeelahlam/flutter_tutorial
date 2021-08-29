9ff9257571976d749a348eab92e053e95a3cfb12
aqeelahlam <aqeelahlam@gmail.com>
2021-08-29 08:23:37 +0530

    chore/Added Gesture Detector

--- lib/screens/locations/locations.dart
+++ lib/screens/locations/locations.dart
@@ -1,1 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tutorial/models/location.dart';
+import 'package:flutter_tutorial/app.dart';
 
+class Locations extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final locations = Location.fetchAll();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Locations'),
+      ),
+      body: ListView(
+        children: locations
+            .map((location) => GestureDetector(
+                  child: Text(location.name),
+                  onTap: () => _onLocationTap(context, location.id),
+                ))
+            .toList(),
+      ),
+    );
+  }
+
+  _onLocationTap(BuildContext context, int locationID) {
+    print("Blah");
+  }
+}
